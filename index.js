19445e07dd0144a3f3890f7a8050acdb9c78c534
milka menachem <milka.elimelech@mail.huji.ac.il>
2022-04-28 17:44:48 +0300

    remove equal cards and flipoff distinct cards

--- index.js
+++ index.js
@@ -22,6 +22,28 @@ let card=document.createElement("div");
 card.textContent = n;
 card.className="card"
 board.appendChild(card);
+card.onclick=flipCard
+card.classList.add("hidden")
 }
-
-
+// debugger
+let twoCards=[]
+function flipCard(event){
+    console.log(event.target);
+    event.target.classList.remove("hidden")
+    twoCards.push(event.target);
+    if (twoCards[1]!=undefined&&isEqual(twoCards)){
+        event.target.classList.remove("card");
+        event.target.textContent="";
+        twoCards[0].classList.remove("card");
+        twoCards[0].textContent="";
+        twoCards=[]
+    }
+    if (twoCards[1]!=undefined&&isEqual(twoCards)==0){
+        setTimeout(()=>{twoCards[0].classList.add("hidden")},500);
+        setTimeout(()=>{twoCards[1].classList.add("hidden")},500);
+        setTimeout(()=>{twoCards=[]},550);
+    }
+}
+function isEqual(twoCards){
+    return  (twoCards[0].textContent==twoCards[1].textContent);
+}
